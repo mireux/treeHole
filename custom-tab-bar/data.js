9428e60327d4d4f0291fa3d872cc858b762e69c0
mireux <799718728@qq.com>
2023-06-18 12:42:56 +0800

    Initial Commit

---
+++ custom-tab-bar/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    icon: 'home',
+    text: '圈子',
+    url: 'pages/home/home',
+  },
+  {
+    icon: 'sort',
+    text: '树洞',
+    url: 'pages/goods/category/index',
+  },
+  {
+    icon: 'cart',
+    text: '购物车',
+    url: 'pages/cart/index',
+  },
+  {
+    icon: 'person',
+    text: '个人中心',
+    url: 'pages/usercenter/index',
+  },
+];
